f541d818b4d1ac3b07ec84483bd405dc027e866c
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-04 01:33:50 -0800

    final 2 fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,12 +5,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    res_add = add(a, b)
-    res_mul = mul(a, b)
-    res_sub = sub(a, b)
-    res_div = div(a, b)
-
-    print(f'{a} + {b} = {res_add}')
-    print(f'{a} * {b} = {res_mul}')
-    print(f'{a} / {b} = {res_div}')
-    print(f'{a} - {b} = {res_sub}')
+    print('{} + {} = {}'.format(a, b, (add(a, b))))
+    print('{} * {} = {}'.format(a, b, (mul(a, b))))
+    print('{} - {} = {}'.format(a, b, (sub(a, b))))
+    print('{} / {} = {}'.format(a, b, (div(a, b))))
