884d44a53ba73d99cd8dd1c70005709eecab467f
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-06 05:21:34 -0800

    fixed IndexErrpr

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list=[]):
     max_num = my_list[0]
 
-    if my_list:
+    if not my_list:
+        return None
+    else:
         for x in my_list:
             if x > max_num:
                 max_num = x
         return max_num
-    else:
-        return None
