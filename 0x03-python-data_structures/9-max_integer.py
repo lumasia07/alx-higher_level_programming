533981b45b3a01eece9fc2818d307280019839bb
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-06 05:14:28 -0800

    max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_num = my_list[0]
+
+    if my_list:
+        for x in my_list:
+            if x > max_num:
+                max_num = x
+        return max_num
+    else:
+        return None
