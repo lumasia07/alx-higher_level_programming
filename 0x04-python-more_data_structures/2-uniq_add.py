e96dd65d8ff63e173e701b67e7a0b5f08b94429f
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-10 01:47:59 -0800

    unique fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,7 @@
 def uniq_add(my_list=[]):
     a = set()
 
-    b = lambda a, i: a.add(a) or a
-    c = reduce(b, my_list, a)
-    return sum(c)
+    for i in my_list:
+        a.add(i)
+
+    return sum(a)
