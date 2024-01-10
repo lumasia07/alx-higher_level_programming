15f51ee51d41a8071944dd191152ed0d0fc05dc5
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-10 04:11:42 -0800

    fixed typo

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     sum = 0
-    num = {'M': 1000, 'D'; 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    num = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for i in reversed(roman_string):
         roman = num[i]
         sum += roman if sum < roman * 5 else -roman
