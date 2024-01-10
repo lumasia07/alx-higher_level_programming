b92b13dfe2425535c826a54014b9dac00b55bca7
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-10 08:58:31 -0800

    square fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda a: list(map(lambda i: i ** 2)), matrix))
+    return list(map((lambda a: list(map((lambda i: i ** i), a))), matrix))
