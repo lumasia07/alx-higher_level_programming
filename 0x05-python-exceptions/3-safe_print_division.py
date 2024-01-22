1c9616a6f30aa44a1014ce1cefe4e751eb1217e8
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-22 01:13:39 -0800

    3 fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,10 +3,8 @@ def safe_print_division(a, b):
     quotient = None
     try:
         quotient = a / b
-    except ZeroDivisionError:
-        print("Division by Zero is no allowed")
-    except Exception as e:
-        print("An expected error occured:", e)
+    except (ZeroDivisionError, TypeError):
+        quotient = None
     finally:
         print("Inside result: {}".format(quotient))
 
