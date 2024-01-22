641faf8fdde5f88ff0572f4d29f0ecdfb9411126
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-22 11:23:37 -0800

    fix

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,9 @@
 #include <Python.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - prints a python list
  * @p: pointer to list to be printed
