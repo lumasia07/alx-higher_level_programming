727e57d35ed3e68a493947a256100a9b75912468
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-22 01:09:39 -0800

    2 fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(val) == int:
                 print("{:d}".format(val), end="")
                 sum += 1
-    except IndexError:
+    except (TypeError, ValueError):
         pass
 
     print()
