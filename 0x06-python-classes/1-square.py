bbb3f3f82da9894c837b852ee022558d41e71dc1
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-23 01:59:54 -0800

    private attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Defines a square"""
+
+
+class Square:
+    """Reps Square"""
+
+    def __init__(self, size):
+        """Initializes this square class
+        Args: size - defines size of square
+        """
+
+        self.__size = size
