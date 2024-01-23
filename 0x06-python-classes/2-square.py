8ef94abc55f93609a67c8498f408526fa5eb83ba
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-23 02:12:46 -0800

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# 2-square.py
+"""Defines a square"""
+
+
+class Square:
+    """A class that reps a square"""
+
+    def __init__(self, size=0):
+        """Inits square class
+        Args:
+            size; reps size of square defined
+        Raises:
+            TypeError: if size is not an int
+            ValueError: if size < 0
+        """
+
+        if not isinstance(size, int):
+            raise Type("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
