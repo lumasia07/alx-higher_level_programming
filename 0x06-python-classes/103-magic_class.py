e4a03eccba997f5b1b61fe7c64c8e1dd96c9a68d
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-24 01:18:16 -0800

    magicclass

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python bytecodes to calculate areas & circumfrence"""
+import math
+
+class MagicClass:
+    """Defines MagicClass"""
+    def __init__(self, radius=0):
+        """Initializes python bytecode
+        Args: radius
+        Raises: TypeError: if radius is non-number
+        """
+        if typr(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be number')
+        self.__radius = radius
+
+        def area(self):
+            """Calculates area
+            Returns: Area
+            """
+            return (self.__radius ** 2 * math.pi)
+
+        def circumfrence(self):
+            """Calculates circumfrence
+            Returns: circumfrence
+            """
+            return (2 * math.pi * self.__radius)
