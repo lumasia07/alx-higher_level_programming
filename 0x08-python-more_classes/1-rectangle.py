19b8aaa46f51a5cef94c0d0c25b38babb387ae53
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-29 11:13:15 -0800

    shebang

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,11 @@
-#!usr/bin/python3
-"""
-
-Module that defines a rectangle
-
-"""
+#!/usr/bin/python3
+"""Module that defines a rectangle"""
 
 
 class Rectangle:
-    """
-    Defines a class Rectangle
-    """
+    """Defines class Rectangle"""
     def __init__(self, width=0, height=0):
+        """Inits class rectangle"""
         self.width = width
         self.height = height
 
