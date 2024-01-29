0367707424fb9dc13228f4cfc3f5ad161827c2ab
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-29 12:11:14 -0800

    rstrip

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Module that defines a rectangle"""
+
+
+class Rectangle:
+    """Defines class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Inits class rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Calculate area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self) -> str:
+        """prints informal str"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rec_str = ""
+
+        for _ in range(self.__height):
+            rec_str += '#' * self.__width + '\n'
+
+        return rec_str.rstrip()
