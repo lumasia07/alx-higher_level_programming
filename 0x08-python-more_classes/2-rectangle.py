0456142d88a716c078a1aa6198394520c349970b
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-29 11:44:50 -0800

    addded 0 case

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,4 +45,6 @@ class Rectangle:
 
     def perimeter(self):
         """Calculate perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
