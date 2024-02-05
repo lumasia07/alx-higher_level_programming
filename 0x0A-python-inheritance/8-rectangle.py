1df8537a969aa3b54e75fc7c6d969b5dfd9c8f61
lumasia07 <lumasiastancelaus@gmail.com>
2024-02-05 11:33:45 -0800

    8 fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,6 +13,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """Defines a rectangle"""
     def __init__(self, width, height):
@@ -20,5 +21,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-
