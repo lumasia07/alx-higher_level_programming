d4b51c450d4e72c0d63dd411c9f3b8f5fba2b7b8
lumasia07 <lumasiastancelaus@gmail.com>
2024-02-05 09:47:57 -0800

    issubclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to check if an instance of class is inherited"""
+
+
+def inherits_from(obj, a_class):
+    """Returns true, false otherwise"""
+    return issubclass(type(obj), a_class)
