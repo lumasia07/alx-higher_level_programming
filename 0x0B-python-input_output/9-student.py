d58fc4eb9b788841d17912d2aafe783e98e55b94
lumasia07 <lumasiastancelaus@gmail.com>
2024-02-06 09:39:01 -0800

    9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to define a class Student"""
+
+
+class Student:
+    """Reps a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Inits a new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns Dict in JSON form"""
+        return self.__dict__
