46b709dc2e61080f8b387e6c19a075b979dc4b31
lumasia07 <lumasiastancelaus@gmail.com>
2024-02-08 11:05:44 -0800

    str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,7 @@ class Rectangle(Base):
         """Display with #"""
         for _ in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """Returns the string rep of the rectangle instance"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
