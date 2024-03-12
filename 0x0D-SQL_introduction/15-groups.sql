c06c24137c528507598b9d8aa519cf51aa1d568b
lumasia07 <lumasiastancelaus@gmail.com>
2024-03-12 06:17:22 -0700

    15 fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Lists records with same score on table
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score
-GROUP BY number DESC;
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
