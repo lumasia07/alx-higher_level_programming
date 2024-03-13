f90046c85705fe001f07cc03c4bd624cceb949ca
lumasia07 <lumasiastancelaus@gmail.com>
2024-03-13 13:44:33 -0700

    states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Creates a database and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF EXISTS hbtn_0d_usa.states(
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
