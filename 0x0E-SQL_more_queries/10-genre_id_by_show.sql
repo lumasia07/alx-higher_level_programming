f273a17b8eebaf8184428763527f1429cb130038
lumasia07 <lumasiastancelaus@gmail.com>
2024-03-14 02:30:42 -0700

    fixes

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Lists all cities contained in the database
 SELECT tv_shows.title, tv_show_genres.genre_id
 	FROM tv_shows
-	INNER JOIN tv_shows_genres
+	JOIN tv_shows_genres
 	ON tv_shows.id = tv_show_genres.show_id
-	ORDER BY tv_shows.title = tv_show_genres.genre_id;
+	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
