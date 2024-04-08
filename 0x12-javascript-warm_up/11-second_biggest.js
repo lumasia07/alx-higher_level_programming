ca3595575b1fe0ad0aec02a1762c6528f6a759e3
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 06:44:27 -0700

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,8 @@ const i = args.map(x => parseInt(x)).filter(x => !isNaN(x));
 
 i.sort((a, b) => b - a);
 
-if (i.length < 2)
-	console.log(0);
-else
-	console.log(i[1]);
+if (i.length < 2) {
+  console.log(0);
+} else {
+  console.log(i[1]);
+}
