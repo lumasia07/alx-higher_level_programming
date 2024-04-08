9f3e4dd724bf4f7a887b7d30356a8ae1a25d0a74
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 00:54:01 -0700

    7

---
+++ 0x12-javascrpit-warm_up/7-multi_c.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const argOne = process.argv[2];
+const x = parseInt(argOne);
+
+if (!isNaN(x))
+{
+	let i;
+	
+	for (i = 0; i < x; i++)
+	{
+		console.log("C is fun");
+	}
+}
+else
+{
+	console.log("Missing number of occurences");
+}
