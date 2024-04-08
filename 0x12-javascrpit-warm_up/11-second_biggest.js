31024dcab01cc6e5c620486b95cccb2ec5d98c44
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 02:27:40 -0700

    largest

---
+++ 0x12-javascrpit-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const i = args.map(x => parseInt(x)).filter(x => !isNaN(x));
+
+i.sort((a, b) => b - a);
+
+if (i.length < 2)
+	console.log(0);
+else
+	console.log(i[1]);
