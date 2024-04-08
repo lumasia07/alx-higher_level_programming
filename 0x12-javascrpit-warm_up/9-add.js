164ae6e5a2f2d670dcb6f2c36f336fb2792e0e39
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 01:44:14 -0700

    add

---
+++ 0x12-javascrpit-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+function add(a, b)
+{
+	return (a + b);
+}
+
+const argOne = parseInt(process.argv[2]);
+const argTwo = parseInt(process.argv[3]);
+
+if (!isNaN(argOne) && !isNaN(argTwo))
+{
+	console.log(add(argOne, argTwo));
+}
+else
+{
+	console.log("Invalid integers");
+}
