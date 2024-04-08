d790d0e59bfc367c338cb6f932a0792a85c0efb2
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 06:11:09 -0700

    7 fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 const argOne = process.argv[2];
 const x = parseInt(argOne);
-
-if (!isNaN(x))
-{
-	let i;
-	
-	for (i = 0; i < x; i++)
-	{
-		console.log("C is fun");
-	}
-}
-else
-{
-	console.log("Missing number of occurences");
+if (!isNaN(x)) {
+  let i;
+  for (i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurences');
 }
