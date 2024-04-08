84ce5597f5d2cbd9a1d8d7966b945cd8370cda26
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 05:31:43 -0700

    2 fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const args = process.argv.length - 2;
-if (args == 0)
-{
-	console.log("No argument");
+
+if (args == 0) {
+  console.log("No argument");
 }
-else if (args == 1)
-{
-	console.log("Argument found");
+else if (args == 1) {
+  console.log("Argument found");
 }
-else
-{
-	console.log("Arguments found");
+else {
+  console.log("Arguments found");
 }
