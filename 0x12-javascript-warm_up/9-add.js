3ded5a875028b6bace296b58f740734cd53c7e63
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 06:33:12 -0700

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,6 +6,4 @@ const argOne = parseInt(process.argv[2]);
 const argTwo = parseInt(process.argv[3]);
 if (!isNaN(argOne) && !isNaN(argTwo)) {
   console.log(add(argOne, argTwo));
-} else {
-  console.log('Invalid integers');
 }
