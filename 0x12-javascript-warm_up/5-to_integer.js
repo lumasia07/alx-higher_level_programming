def8c8dbd9377a65d4be3fad3583fe90ba9904b1
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 05:57:56 -0700

    5 fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const argOne = process.argv[2];
 const args = parseInt(argOne);
-if (!isNaN(args))
-{
-	console.log(`My Number: ${args}`);
-}
-else
-{
-	console.log("Not a number");
+if (!isNaN(args)) {
+  console.log(`My number: ${args}`);
+} else {
+  console.log('Not a number');
 }
