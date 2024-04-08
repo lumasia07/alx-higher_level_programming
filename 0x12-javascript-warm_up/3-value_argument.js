af1fc7f081b3a706015b1321684e6182f8f43607
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 05:47:07 -0700

    3 fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const argOne = process.argv[2];
-if (argOne !== undefined)
-{
-	console.log(argOne);
-}
-else
-{
-	console.log("No argument");
+if (argOne !== undefined) {
+  console.log(argOne);
+} else {
+  console.log("No argument");
 }
