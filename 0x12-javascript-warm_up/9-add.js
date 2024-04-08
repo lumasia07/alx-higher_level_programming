1c7de452210f16abc963c5aced09a18364f8a3d9
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 06:39:32 -0700

    9 fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
 function add (a, b) {
   return (a + b);
 }
-const argOne = parseInt(process.argv[2]);
-const argTwo = parseInt(process.argv[3]);
-if (!isNaN(argOne) && !isNaN(argTwo)) {
-  console.log(add(argOne, argTwo));
-}
+
+console.log(add(a, b));
