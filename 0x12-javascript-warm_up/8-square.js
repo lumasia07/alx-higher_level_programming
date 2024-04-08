73e64bfe287d37f179083ac182c38a6ae899f0a8
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-08 06:28:56 -0700

    11 fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,23 +1,15 @@
 #!/usr/bin/node
 const argOne = process.argv[2];
 const x = parseInt(argOne);
-
-if (!isNaN(x))
-{
-	let i, j;
-
-	for (i = 0; i < x; i++)
-	{
-		let line = "";
-
-		for (j = 0; j < x; j++)
-		{
-			line += "X";
-		}
-		console.log(line);
-	}
-}
-else
-{
-	console.log("Missing size");
+if (!isNaN(x)) {
+  let i, j;
+  for (i = 0; i < x; i++) {
+    let line = '';
+    for (j = 0; j < x; j++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+} else {
+  console.log('Missing size');
 }
