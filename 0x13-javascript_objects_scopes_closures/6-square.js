51305b90d6d0c0e740b1871cb89fb8eaa1c8fa7b
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-09 10:56:36 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,6 @@
 const BaseSquare = require('./5-square');
 
 class Square extends BaseSquare {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
