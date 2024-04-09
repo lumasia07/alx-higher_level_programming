bb5d9e2fef32d557ac7c03465f3ec348d175c8d2
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-09 11:16:35 -0700

    fix

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let revList = [];
+
+  for (let x = list.length - 1; x >=0; x--) {
+    revList.push(list[x]);
+  }
+
+  return (revList);
+};
