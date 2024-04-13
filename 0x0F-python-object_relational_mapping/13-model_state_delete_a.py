9fd96d510ee93792975825ecd568028d48311146
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-13 05:02:57 -0700

    del a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module lists state all state objects"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """Access database and get states"""
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    a_state = session.query(State).filter(State.name.contains('a'))
+
+    if a_state is not None:
+        for state in a_state:
+            session.delete(state)
+
+    session.commit()
+
+    session.close()
