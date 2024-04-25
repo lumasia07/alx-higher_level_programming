7675507b386b74901f3c3c99d5450c1268bfd274
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-25 11:35:06 -0700

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers """
+
+
+def find_peak(list_of_integers):
+    """
+    Defined a function find_peak.
+
+    Args:
+    - list of integers: A list of unsorted integers.
+
+    Returns:
+    - Peak element in the list
+    """
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if  list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
