eec943d8ff1ee4bbd3fecb86a43a50f17bb73c90
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-26 02:24:07 -0700

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,12 +9,13 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-email = sys.argv[2]
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('utf-8')
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
 
-with urllib.request.urlopen(url, data=data) as response:
-    encoding = response.read().decode('utf-8')
+    with urllib.request.urlopen(url, data=data) as response:
+        encoding = response.read().decode('utf-8')
 
-    print(encoding)
+        print(encoding)
