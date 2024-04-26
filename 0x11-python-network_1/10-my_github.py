02a9b25a8bf0ccb85ea298d089061a42c0288f4e
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-26 04:39:28 -0700

    github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that uses Github API to display credentials"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
