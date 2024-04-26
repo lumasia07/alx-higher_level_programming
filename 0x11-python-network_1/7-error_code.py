f596bbf3558e182d2155d86940cd541a76df6020
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-26 03:40:18 -0700

    fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,10 +9,13 @@ import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    try:
+        url = sys.argv[1]
 
-    req = requests.get(url)
-    if req.status_code >= 400:
-        print("Error code: {}".format(req.status_code))
-    else:
-        print(req.text)
+        req = requests.get(url)
+        if req.status_code >= 400:
+            print("Error code: {}".format(req.status_code))
+        else:
+            print(req.text)
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
