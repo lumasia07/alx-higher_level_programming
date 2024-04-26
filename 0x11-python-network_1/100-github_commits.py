e45bdce7cb2324e91a39e2eebe1a7855b1eee21e
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-26 04:55:55 -0700

    okay

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists 10 most recenet commits from a repo"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    req = requests.get(url)
+    commits =req.json()
+
+    try:
+        for _ in range(10):
+            print("{}: {}".format(
+                commits[_].get("sha"),
+                commits[_].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
