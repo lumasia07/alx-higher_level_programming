42c3dac939fe1f7a072a116c35769b533ae1d10e
lumasia07 <lumasiastancelaus@gmail.com>
2024-05-22 12:46:18 -0700

    0

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let c = 0;
+    const films = JSON.parse(body).results;
+    for (let res = 0; res < films.length; res++) {
+      const chts = films[res].characters;
+      for (let i = 0; i < chts.length; i++) {
+        if (chts[i] === 'https://swapi-api.hbtn.io/api/people/18/') {
+	  c += 1;
+	}
+      }
+    }
+    console.log(c);
+  }
+});
