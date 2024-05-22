1613fc5d5a331ca9cae0aafb6019e398cbf677af
lumasia07 <lumasiastancelaus@gmail.com>
2024-05-22 06:18:43 -0700

    0-readme

---
+++ 0x14-javascript-web_scrapping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2]
+
+if (!filePath) {
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else {
+   console.log(data);
+  }
+});
